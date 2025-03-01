ec4278ce4ca3f959dfdf6f11c65259a543391d59
Barrett Pyke <barrettpyke@gmail.com>
2025-02-28 20:35:09 -0600

    make header a link fix css on product page to be responsive

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -13,7 +13,7 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
   const { name, description, price } = product;
 
   return (
-    <div className="flex justify-between mx-20">
+    <div className="flex justify-between mx-20 xl:mx-60 gap-10">
       <Image
         src={`/products/${id}.avif`}
         alt={`Picture of product named ${name}`}
@@ -21,10 +21,13 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
         height={2000}
         className="max-w-xl max-h-md mb-4"
       />
-      <div className="flex flex-col gap-5 m-5">
+      <div className="flex flex-col my-10 ml-15">
         <h1 className="text-xl">{name}</h1>
         <div>${price}.00</div>
-        <div className="max-w-1/2">{description}</div>
+        <div className="max-w-3/4 mt-3">{description}</div>
+        <button className="w-40 bg-emerald-500 hover:bg-emerald-700 cursor-pointer text-white font-bold py-3 px-4 rounded-none mt-10">
+          Add to Cart
+        </button>
       </div>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 import { ShoppingBagIcon } from './icons';
 
 const Header: React.FC = () => {
   return (
     <div className="flex justify-between items-center mb-8">
-      <h1 className="text-xl mx-auto">Cozy Threads</h1>
+      <Link className="mx-auto" href="/">
+        <h1 className="text-xl">Cozy Threads</h1>
+      </Link>
       <ShoppingBagIcon className="mr-9" />
     </div>
   );
