22df16ae777d22f4e38924fa0aaad841111eb0dd
Barrett Pyke <barrettpyke@gmail.com>
2025-03-02 16:37:49 -0600

    update return statement of checkout page

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,13 +1,21 @@
 'use client';
 
+import LoadingSpinner from '@/components/LoadingSpinner';
 import dynamic from 'next/dynamic';
 
 const DynamicComponent = dynamic(() => import('../../components/CheckoutPage'), {
   ssr: false,
+  loading: () => (
+    <div className="flex justify-center mt-30">
+      <LoadingSpinner width={5} height={5} />
+    </div>
+  ),
 });
 
 export default function Page() {
-  <div>
-    <DynamicComponent />
-  </div>;
+  return (
+    <div>
+      <DynamicComponent />
+    </div>
+  );
 }
